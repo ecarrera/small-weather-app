4f728c9a63ab16152ea418f8eba5c85aefb4d266
Emisael Carrera <emisael.carrera@gmail.com>
2024-09-17 00:45:07 -0300

    chore: initial commit
    - add vite, vue, eslint and typescript config files
    - add packages

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import eslintPluginVue from "eslint-plugin-vue";
+import ts from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default ts.config(
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...eslintPluginVue.configs["flat/recommended"],
+  {
+    files: ["*.vue", "**/*.vue"],
+    languageOptions: {
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+      },
+    },
+  },
+  eslintPluginPrettierRecommended
+);
